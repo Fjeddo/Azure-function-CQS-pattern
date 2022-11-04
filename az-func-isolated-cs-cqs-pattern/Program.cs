f23cdd55cd99b44ef109dd3818c229a277174b4b
Fredde Johnsson <fredrik.johnsson7@gmail.com>
2022-11-04 14:01:56 +0100

    Add extension methods for response data; add middlewares

--- az-func-isolated-cs-cqs-pattern/Program.cs
+++ az-func-isolated-cs-cqs-pattern/Program.cs
@@ -8,10 +8,108 @@ var host = new HostBuilder()
     .ConfigureServices(services =>
     {
         services.AddSingleton<IQueryExecuter, QueryExecuter>()
-                .AddSingleton<ICommandHandler, CommandHandler>()
-                .AddScoped<UpdateUserProcess>()
-                .AddScoped<IUserStorage, FakeUserStorage>();
+            .AddSingleton<ICommandHandler, CommandHandler>()
+            .AddScoped<UpdateUserProcess>()
+            .AddScoped<IUserStorage, FakeUserStorage>();
     })
     .Build();
 
 host.Run();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+        builder.UseWhen(
+            _ => DateTimeOffset.Now.Second % 2 == 0,
+            async (context, _) =>
+            {
+                if (await context.GetHttpRequestDataAsync() is {} req)
+                {
+                    context.GetInvocationResult().Value = req.CreateResponse(HttpStatusCode.UnavailableForLegalReasons);
+                }
+            }
+        );
+
+
+
+--------------
+
+
+        builder.UseMiddleware(async (context, next) =>
+        {
+            await next();
+
+            var logger = context.InstanceServices.GetService<ILogger<Program>>();
+            logger.LogInformation("----------------> END");
+        });
+
+        builder.UseMiddleware(async (context, next) =>
+        {
+            var logger = context.InstanceServices.GetService<ILogger<Program>>();
+
+            logger.LogInformation("START1 ------------>");
+            await next();
+        });
+
+        builder.UseMiddleware(async (context, next) =>
+        {
+            var logger = context.InstanceServices.GetService<ILogger<Program>>();
+
+            logger.LogInformation("START2 ------------>");
+            await next();
+        });
+
+
+
+
+*/
--- az-func-isolated-cs-cqs-pattern/UpdateUser.cs
+++ az-func-isolated-cs-cqs-pattern/UpdateUser.cs
@@ -3,23 +3,20 @@ using az_func_cs_cqs_pattern.Code;
 using az_func_cs_cqs_pattern.Code.Models;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
-using Microsoft.Extensions.Logging;
 
 namespace az_func_isolated_cs_cqs_pattern;
 
 public class UpdateUser
 {
-    private readonly ILogger _logger;
     private readonly UpdateUserProcess _updateUserProcess;
 
-    public UpdateUser(UpdateUserProcess updateUserProcess, ILoggerFactory loggerFactory)
+    public UpdateUser(UpdateUserProcess updateUserProcess)
     {
-        _logger = loggerFactory.CreateLogger<UpdateUser>();
         _updateUserProcess = updateUserProcess;
     }
 
-    [Function("Function1")]
-    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+    [Function("UpdateUser")]
+    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, methods: "post", Route = "user")] HttpRequestData req)
     {
         var updateUserRequest = await req.ReadFromJsonAsync<UpdateUserRequest>();
 
@@ -37,5 +34,17 @@ public class UpdateUser
             var response = req.CreateResponse((HttpStatusCode)status);
             return response;
         }
+    }
+
+    [Function("UpdateUserExt")]
+    public async Task<HttpResponseData> RunExt([HttpTrigger(AuthorizationLevel.Function, methods: "post", Route = "userExt")] HttpRequestData req)
+    {
+        var updateUserRequest = await req.ReadFromJsonAsync<UpdateUserRequest>();
+
+        var (success, model, status) = await _updateUserProcess.Run(updateUserRequest);
+
+        return success
+            ? await req.CreateOkObjectResult(model)
+            : await req.CreateStatusCodeResult(status);
     }
 }
