437b97e7b851c034eee165179a0c74557c197ecf
Fredde Johnsson <fredrik.johnsson7@gmail.com>
2022-11-07 22:03:12 +0100

    Migration to isolated done

---
+++ az-function-cs-cqs-pattern-isolated/Program.cs
@@ -0,0 +1,26 @@
+using az_functions_cs_cqs_pattern.Code;
+using az_functions_cs_cqs_pattern.Code.FakeDb;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var host = new HostBuilder()
+    .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices(services =>
+        services
+            .AddSingleton<IQueryExecuter, QueryExecuter>()
+            .AddSingleton<ICommandHandler, CommandHandler>()
+            .AddScoped<UpdateUserProcess>()
+            .AddScoped<IUserStorage, FakeUserStorage>())
+    .Build();
+
+host.Run();
+
+
+/*
+void AddProjectDependencies(IServiceCollection services) =>
+    services
+        .AddSingleton<QueryExecuter>()
+        .AddSingleton<CommandHandler>()
+        .AddScoped<UpdateUserProcess>()
+        .AddScoped<IUserStorage, FakeUserStorage>();
+*/
---
+++ az-function-cs-cqs-pattern-isolated/UpdateUser.cs
@@ -0,0 +1,39 @@
+using System.Net;
+using az_functions_cs_cqs_pattern.Code;
+using az_functions_cs_cqs_pattern.Code.Models;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+
+namespace az_function_cs_cqs_pattern_isolated
+{
+    public class UpdateUser
+    {
+        private readonly UpdateUserProcess _updateUserProcess;
+
+        public UpdateUser(UpdateUserProcess updateUserProcess)
+        {
+            _updateUserProcess = updateUserProcess;
+        }
+
+        [Function("UpdateUser")]
+        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = "user")] HttpRequestData req)
+        {
+            var updateUserRequest = await req.ReadFromJsonAsync<UpdateUserRequest>();
+
+            var (success, model, status) = await _updateUserProcess.Run(updateUserRequest);
+
+            if (success)
+            {
+                var response = req.CreateResponse(HttpStatusCode.OK);
+                await response.WriteAsJsonAsync(model);
+
+                return response;
+            }
+            else
+            {
+                var response = req.CreateResponse((HttpStatusCode)status);
+                return response;
+            }
+        }
+    }
+}
