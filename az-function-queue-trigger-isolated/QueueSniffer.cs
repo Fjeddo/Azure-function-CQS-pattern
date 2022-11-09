f0140bb2e110e50b0439ce26681a22f16f88af1c
Fredde Johnsson <fredrik.johnsson7@gmail.com>
2022-11-09 06:44:10 +0100

    Add queue sniffer function with a middleware

---
+++ az-function-queue-trigger-isolated/QueueSniffer.cs
@@ -0,0 +1,22 @@
+using System;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace az_function_queue_trigger_isolated
+{
+    public class QueueSniffer
+    {
+        private readonly ILogger _logger;
+
+        public QueueSniffer(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<QueueSniffer>();
+        }
+
+        [Function("QueueSniffer")]
+        public void Run([QueueTrigger("notify", Connection = "AzureWebJobsStorage")] string myQueueItem)
+        {
+            _logger.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+        }
+    }
+}
